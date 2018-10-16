7b0cc3f7dbb8efd92c9bff038fea645ea8d43965
Alex Tereshenkov <AlexArcPy@users.noreply.github.com>
2018-10-16 19:39:28 +0200

    Adding stuff from alex updates into Daves repo (#2)
    * Alex added new line on line 5 in github web UI
    
    * Alex added new line on line 6 in github web UI
    
    * Alex added new line on line 8 in desktop app
    
    * Alex added new line on line 7 in github web UI
    
    * Alex added new line on line 10 in desktop app
    
    * Alex added new line on line 9 in github web UI

--- sample.py
+++ sample.py
@@ -1,3 +1,10 @@
 # Dave added new line on line 1
 # Dave added new line on line 2
 # Alex added new line on line 3
+
+# Alex added new line on line 5 in github web UI
+# Alex added new line on line 6 in github web UI
+# Alex added new line on line 7 in github web UI
+
+# Alex added new line on line 9 in github web UI
+# Alex added new line on line 10 in desktop app
