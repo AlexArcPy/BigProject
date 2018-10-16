3bb1b7f6e84a9f12bed9941c0f70e7ed8cd072d6
Alex Tereshenkov <avtereshenkov@yahoo.com>
2018-10-16 11:27:10 +0200

    Alex added new line on line 10 in desktop app

--- sample.py
+++ sample.py
@@ -5,3 +5,6 @@
 # Alex added new line on line 5 in github web UI
 # Alex added new line on line 6 in github web UI
 # Alex added new line on line 7 in github web UI
+
+
+# Alex added new line on line 10 in desktop app
