52b8a37732097adf1d3701c2da9a59a95de28af2
Alex Tereshenkov <avtereshenkov@yahoo.com>
2019-03-09 22:10:13 +0000

    Local update file

--- file1.py
+++ file1.py
@@ -1,2 +1,12 @@
 foo = 5  # local update file1 - again locally
 foo = 50  # remote - again remotely
+
+
+def foo(x):
+    """Docstring DocstringDocstringDocstringDocstringDocstring Docstring Docst
+    ringDocstring
+
+    Args:
+        x:
+    """
+    pass
